d4657afdb85f16399709daa58882441c6532e584
Michael B. Bañaria <mbbmichaelbolobanaria25@gmail.com>
2025-07-02 11:51:19 +0800

    Create if statements.

--- src/App.java
+++ src/App.java
@@ -1,5 +1,31 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        if (20 > 18) {
+            System.out.println("Hello World!");
+        }
+
+        int number1 = 20, number2 = 18;
+        if (number1 > number2) {
+            System.out.println("Hello World!");
+        }
+
+        if (number1 < number2) {
+            System.out.println("Hello World!");
+        } else {
+            System.out.println("Hello Programming");
+        }
+
+        int time = 22;
+        if (time < 10) {
+            System.out.println("Good morning");
+        } else if (time < 18) {
+            System.out.println("Good day");
+        } else {
+            System.out.println("Good evening");
+        }
+
+        int timestamp = 20;
+        String result = (timestamp < 18) ? "Good day" : "Good evening";
+        System.out.println(result);
     }
 }
