88eb562d0e6969fbf7e35e26dc3395a540e6aae5
Michael B. Bañaria <mbbmichaelbolobanaria25@gmail.com>
2025-07-02 14:58:14 +0800

    Create while-do-while.

--- src/App.java
+++ src/App.java
@@ -1,5 +1,17 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int number = 0;
+
+        while (number < 5) {
+        System.out.println(number);
+        number++;
+        }
+        System.out.println("Loop finished.");
+
+        // do {
+        //     System.out.println("This will always execute at least once.");
+        //     number++;
+        // } while (number < 5);
+        // System.out.println("Loop finished.");
     }
 }
