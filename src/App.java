639d57f2910d79544719d9717e41f031428143a9
Michael B. Bañaria <mbbmichaelbolobanaria25@gmail.com>
2025-07-02 11:42:38 +0800

    Add operators.

--- src/App.java
+++ src/App.java
@@ -1,5 +1,17 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int addition = 5 + 5;
+        System.out.println(addition);
+        int addon = addition + 5;
+        System.out.println(addon);
+        int solve = addon + addon;
+        System.out.println(solve);
+        System.out.println(++solve);
+        System.out.println(--solve);
+
+        int x = 5;
+        int y = 3;
+        System.out.println(x > y);
+        System.out.println(x > 3 && x < 10);
     }
 }
