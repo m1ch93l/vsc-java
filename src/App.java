c5e1ee6c333664b120c5e2d98a9b3b59e1de0bb5
Michael B. Bañaria <mbbmichaelbolobanaria25@gmail.com>
2025-07-02 15:10:10 +0800

    Create outer and inner for loop.

--- src/App.java
+++ src/App.java
@@ -1,17 +1,18 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        int number = 0;
 
-        while (number < 5) {
-        System.out.println(number);
-        number++;
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Current number: " + i);
         }
-        System.out.println("Loop finished.");
 
-        // do {
-        //     System.out.println("This will always execute at least once.");
-        //     number++;
-        // } while (number < 5);
-        // System.out.println("Loop finished.");
+        // // Outer loop
+        // for (int i = 1; i <= 2; i++) {
+        //     System.out.println("Outer: " + i); // Executes 2 times
+
+        //     // Inner loop
+        //     for (int j = 1; j <= 3; j++) {
+        //         System.out.println("---Inner: " + j); // Executes 6 times (2 * 3)
+        //     }
+        // }
     }
 }
