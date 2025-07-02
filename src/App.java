1aa0a8f489ce032459ffc10cc0bf717b4df852c8
Michael B. Bañaria <mbbmichaelbolobanaria25@gmail.com>
2025-07-02 11:39:51 +0800

    Create datatypes with variables.

--- src/App.java
+++ src/App.java
@@ -1,5 +1,24 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        String text = "Michael B. Banaria";
+        int number = 100;
+        float decimal = 99.9f;
+        char single_character = 'M';
+        boolean true_or_false = true;
+
+        System.out.println(text);
+        System.out.println(number);
+        System.out.println(decimal);
+        System.out.println(single_character);
+        System.out.println(true_or_false);
+
+        String firstname = "Michael ";
+        String lastname = "Banaria";
+        String fullname = firstname + lastname;
+
+        System.out.println(fullname);
+
+        String purok = "Zone 1 ", barangay = "San Juan ", municipality = "Baao";
+        System.out.print(purok + barangay + municipality);
     }
 }
