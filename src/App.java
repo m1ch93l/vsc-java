c3ca90cc87ec6269d49295587cba77f1f6b64645
m1ch93l <mbbmichaelbolobanaria25@gmail.com>
2025-07-25 09:38:30 +0800

    Refactor App.java to uncomment and organize color array initialization

--- src/App.java
+++ src/App.java
@@ -1,18 +1,43 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        String[] colors;
+        // String[] colors;
 
-        // String[] colors = {
-        //         "red",
-        //         "green",
-        //         "blue",
-        //         "yellow",
-        //         "purple"
-        // };
+        String[] colors = {
+                "red",
+                "green",
+                "blue",
+                "yellow",
+                "purple"
+        };
+
+        System.out.println(colors[0]);
+        System.out.println(colors.length);
 
         // int[] numbers = { 1, 2, 3, 4, 5 };
 
-        // System.out.println(colors[0]);
-        // System.out.println(colors.length);
+        // // 2D array
+        // int[][] twoDArray = {
+        // { 1, 2, 3 },
+        // { 4, 5, 6 },
+        // { 7, 8, 9 }
+        // };
+        // // Accessing elements in a 2D array
+        // System.out.println(twoDArray[0][0]); // Output: 1
+        // System.out.println(twoDArray[1][1]); // Output: 5
+
+        // // 3D array
+        // int[][][] threeDArray = {
+        // {
+        // { 1, 2, 3 },
+        // { 4, 5, 6 }
+        // },
+        // {
+        // { 7, 8, 9 },
+        // { 10, 11, 12 }
+        // }
+        // };
+        // // Accessing elements in a 3D array
+        // System.out.println(threeDArray[0][0][0]); // Output: 1
+        // System.out.println(threeDArray[1][1][2]); // Output: 12
     }
 }
