3ba7da76fbcaaa91acbd4903ff73bfee7f810a06
m1ch93l <mbbmichaelbolobanaria25@gmail.com>
2025-07-25 10:29:40 +0800

    Implement user input for personalized greeting in App.java

--- src/App.java
+++ src/App.java
@@ -1,5 +1,10 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter your name: ");
+        String username = input.nextLine();
+        System.out.println("Hello " + username);
     }
 }
