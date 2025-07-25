711c35a007c5c09303e4dabb133653bdb0dd5d97
m1ch93l <mbbmichaelbolobanaria25@gmail.com>
2025-07-25 09:15:18 +0800

    Update loop condition and add delay in main method

--- src/App.java
+++ src/App.java
@@ -2,33 +2,34 @@ public class App {
     public static void main(String[] args) throws Exception {
         int number = 0;
 
-        while (number < 5) {
+        while (number <= 5) {
             System.out.println(number);
             number++;
+            Thread.sleep(500);
         }
         System.out.println("Loop finished.");
 
         // do {
-        //     System.out.println("This will always execute at least once.");
-        //     number++;
+        // System.out.println("This will always execute at least once.");
+        // number++;
         // } while (number < 5);
         // System.out.println("Loop finished.");
 
         // while (number < 10) {
-        //     System.out.println(number);
-        //     number++;
-        //     if (number == 4) {
-        //         break;
-        //     }
+        // System.out.println(number);
+        // number++;
+        // if (number == 4) {
+        // break;
+        // }
         // }
 
         // while (number < 10) {
-        //     if (number == 4) {
-        //         number++;
-        //         continue;
-        //     }
-        //     System.out.println(number);
-        //     number++;
+        // if (number == 4) {
+        // number++;
+        // continue;
+        // }
+        // System.out.println(number);
+        // number++;
         // }
     }
 }
