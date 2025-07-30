00e5bc1fc75108964bc3de1faa42713000dbe0af
Michael B. Bañaria <mbbmichaelbolobanaria25@gmail.com>
2025-07-30 21:59:16 +0800

    Refactor main method comments for clarity

--- src/App.java
+++ src/App.java
@@ -1,5 +1,6 @@
 public class App {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws Exception { // Entry point of the application
+        // Print a greeting message to the console
         System.out.println("Hello, World!");
     }
 }
