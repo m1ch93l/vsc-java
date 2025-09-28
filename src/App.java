05ee6b62faf903e09d326d6e4b318ebec11f09f3
Michael B. Bañaria <mbbmichaelbolobanaria25@gmail.com>
2025-09-28 12:06:06 +0800

    Revert default

--- src/App.java
+++ src/App.java
@@ -1,24 +1,6 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        String text = "Michael B. Banaria";
-        int number = 100;
-        float decimal = 99.9f;
-        char single_character = 'M';
-        boolean true_or_false = true;
-
-        System.out.println(text);
-        System.out.println(number);
-        System.out.println(decimal);
-        System.out.println(single_character);
-        System.out.println(true_or_false);
-
-        String firstname = "Michael ";
-        String lastname = "Banaria";
-        String fullname = firstname + lastname;
-
-        System.out.println(fullname);
-
-        String purok = "Zone 1 ", barangay = "San Juan ", municipality = "Baao";
-        System.out.print(purok + barangay + municipality);
+    public static void main(String[] args) throws Exception { // Entry point of the application
+        // Print a greeting message to the console
+        System.out.println("Hello, World!");
     }
 }
