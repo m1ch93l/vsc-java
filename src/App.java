20a5b9e41be021a487605452339af804da039ec9
Michael B. Bañaria <mbbmichaelbolobanaria25@gmail.com>
2025-09-28 12:26:31 +0800

    Replace and add java class for operators

--- src/App.java
+++ src/App.java
@@ -1,17 +1,6 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        int addition = 5 + 5;
-        System.out.println(addition);
-        int addon = addition + 5;
-        System.out.println(addon);
-        int solve = addon + addon;
-        System.out.println(solve);
-        System.out.println(++solve);
-        System.out.println(--solve);
-
-        int x = 5;
-        int y = 3;
-        System.out.println(x > y);
-        System.out.println(x > 3 && x < 10);
+    public static void main(String[] args) throws Exception { // Entry point of the application
+        // Print a greeting message to the console
+        System.out.println("Hello, World!");
     }
 }
